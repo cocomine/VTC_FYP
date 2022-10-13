677f35ddb5fc16dfbf58bbb1cbf5a4ad5524184b
cocomine <roguish.6888467468@gmail.com>
2022-10-14 01:41:59 +0800

    auto clear database

--- src/panel/function/auto_run/clearDataBase.php
+++ src/panel/function/auto_run/clearDataBase.php
@@ -4,7 +4,16 @@
  * Create by cocomine
  */
 
-use phpbrowscap\Browscap;
-
 require_once ('../config.inc.php');
 
+$sqlcon = new mysqli(Cfg_Sql_Host, Cfg_Sql_dbUser, Cfg_Sql_dbPass, Cfg_Sql_dbName);
+$stmt = $sqlcon->prepare("TRUNCATE Block_ip");
+$stmt->execute();
+$stmt->prepare('DELETE FROM Toke_list WHERE UNIX_TIMESTAMP()-Time > 1209600');
+$stmt->execute();
+$stmt->prepare('DELETE FROM ForgetPass WHERE UNIX_TIMESTAMP()-Last_time > 3600');
+$stmt->execute();
+$stmt->prepare('DELETE FROM User WHERE UNIX_TIMESTAMP()-Last_Login > 86400 AND activated IS FALSE');
+$stmt->execute();
+$stmt->prepare('DELETE FROM notify WHERE UNIX_TIMESTAMP()-Time > 777600');
+$stmt->execute();
