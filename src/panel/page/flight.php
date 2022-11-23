3072d763e1422dfde130e91a6a88e55c7ae034ac
cocomine <roguish.6888467468@gmail.com>
2022-11-23 19:40:45 +0800

    預約座位 2/2 + 語言文件

--- src/panel/page/flight.php
+++ src/panel/page/flight.php
@@ -16,6 +16,7 @@ class flight implements IPage {
     private mysqli $sqlcon;
     private array $upPath;
     private string $flight;
+    private bool $isAuth;
 
     /**
      * @param mysqli $sqlcon
@@ -27,21 +28,25 @@ class flight implements IPage {
     }
 
     public function access(bool $isAuth, int $role, bool $isPost): int {
-        if($isPost && !$isAuth) return 401;
-        if(sizeof($this->upPath) != 1) return 404;
+        if ($isPost && !$isAuth) return 401;
+        if (sizeof($this->upPath) != 1) return 404;
 
         /* 是否在本日之後 */
         $stmt = $this->sqlcon->prepare("SELECT Flight FROM Flight WHERE ID = ? AND DateTime >= CURRENT_DATE");
         $stmt->bind_param('s', $this->upPath[0]);
-        if(!$stmt->execute()) return 500;
+        if (!$stmt->execute()) return 500;
 
         $result = $stmt->get_result();
-        if(!$result->num_rows > 0) return 404; //不是本日期之後
+        if (!$result->num_rows > 0) return 404; //不是本日期之後
         $this->flight = $result->fetch_assoc()['Flight'];
+        $this->isAuth = $isAuth;
         return 200;
     }
 
     public function showPage(): string {
+        global $auth;
+        $fmt = numfmt_create('zh', NumberFormatter::DECIMAL);
+
         /* 取得資料 */
         $stmt = $this->sqlcon->prepare(
             "SELECT Flight.Flight, Flight.DateTime, Flight.`From`, Flight.`To`, Price.Economy AS PriceEconomy, Price.Business AS PriceBusiness, 
@@ -51,7 +56,7 @@ class flight implements IPage {
             (SELECT Name FROM Location WHERE Code = Flight.`To`) AS ToStr
         FROM Flight, Price, Aircaft WHERE Flight.ID = ? AND Price.ID = Flight.ID AND Flight.Aircaft = Aircaft.ID");
         $stmt->bind_param('s', $this->upPath[0]);
-        if(!$stmt->execute()) return "";
+        if (!$stmt->execute()) return "";
 
         /* 處理資料 */
         $result = $stmt->get_result();
@@ -71,16 +76,46 @@ class flight implements IPage {
             'Business' => $row['Business'],
         ));
 
-        $fmt = numfmt_create( 'zh', NumberFormatter::DECIMAL);
+        /* 檢查是否已預約 */
+        $isReserve = array(
+            'css' => '',
+            'alert' => 'd-none',
+            'Business' => 0,
+            'Economy' => 0,
+            'Meal' => '',
+            'button' => '',
+            'total' => '0'
+        );
+        if ($this->isAuth) {
+            $stmt = $this->sqlcon->prepare("SELECT * FROM Reserve WHERE UUID = ? AND ID = ?");
+            $stmt->bind_param("si", $auth->userdata['UUID'], $this->upPath[0]);
+            if (!$stmt->execute()) return "";
+
+            $result = $stmt->get_result();
+            if($result->num_rows >= 1){
+                $tmp = $result->fetch_assoc();
+                $isReserve['css'] = 'cover';
+                $isReserve['alert'] = '';
+                $isReserve['Business'] = $tmp['Business'];
+                $isReserve['Economy'] = $tmp['Economy'];
+                $isReserve['Meal'] = $tmp['Meal'] ? 'checked' : '';
+                $isReserve['button'] = 'disabled';
+                $isReserve['total'] = $fmt->format(($row['PriceBusiness'] * $tmp['Business'] + $row['PriceEconomy'] * $tmp['Economy']));;
+            }
+        }
+
+        /* format */
         $row['PriceBusiness'] = $fmt->format($row['PriceBusiness']);
         $row['PriceEconomy'] = $fmt->format($row['PriceEconomy']);
 
+        /* json */
         $Text = showText('Flight.Content');
         $LangJson = json_encode(array(
             'Need_reserve' => $Text['Confirm_Reserve']['Need_reserve'],
             'No_Need_reserve' => $Text['Confirm_Reserve']['No_Need_reserve'],
             'Need_login' => $Text['Need_login']
         ));
+
         return <<<body
 <!--pre id='langJson' style='display: none'>{}</pre-->
 <pre id='DataJson' style='display: none'>$dataJson</pre>
@@ -109,16 +144,17 @@ class flight implements IPage {
             </div>
         </div>
         <div class='col-12'>
-            <div class="card" id="Reserve">
+            <div class="card {$isReserve['css']}" id="Reserve">
                 <div class="card-body">
                     <h5 class="header-title">{$Text['Reserve_Seat']}</h5>
+                    <div class="alert alert-warning {$isReserve['alert']}" style="z-index: 10" role="alert"><i class="fa-solid fa-triangle-exclamation me-2"></i>{$Text['alert']}</div>
                     <div style="background-color: lightgray" class="rounded p-1">
                         <div class="row justify-content-sm-between align-items-center justify-content-center">
                             <h5 class="col-auto">{$Text['Cabin_type'][1]}</h5>
                             <div class="col-auto">
                                 <div class="row align-items-center">
                                     <div class="col-auto"><button type="button" class="btn btn-primary btn-rounded" data-reserve="Business-add"><i class="fa-solid fa-plus"></i></button></div>
-                                    <h6 class="col-auto" id="Business-count">0</h6>
+                                    <h6 class="col-auto" id="Business-count">{$isReserve['Business']}</h6>
                                     <div class="col-auto"><button type="button" class="btn btn-outline-primary btn-rounded" data-reserve="Business-sub"><i class="fa-solid fa-minus"></i></button></div>
                                 </div>
                             </div>
@@ -130,25 +166,25 @@ class flight implements IPage {
                             <div class="col-auto">
                                 <div class="row align-items-center">
                                     <div class="col-auto"><button type="button" class="btn btn-primary btn-rounded" data-reserve="Economy-add"><i class="fa-solid fa-plus"></i></button></div>
-                                    <h6 class="col-auto" id="Economy-count">0</h6>
+                                    <h6 class="col-auto" id="Economy-count">{$isReserve['Economy']}</h6>
                                     <div class="col-auto"><button type="button" class="btn btn-outline-primary btn-rounded" data-reserve="Economy-sub"><i class="fa-solid fa-minus"></i></button></div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div style="background-color: lightgray" class="mt-2 rounded p-1">
+                    <div style="background-color: lightgray" class="mt-2 rounded p-1 py-2">
                         <div class="row justify-content-sm-between align-items-center justify-content-center">
                             <h5 class="col-auto">{$Text['reserve_meal']}</h4>
-                            <div class="col-auto me-sm-2">
-                                <input class="form-check-input mt-0" style="font-size: 2rem" type="checkbox" id="Meal">
+                            <div class="col-auto form-check form-switch">
+                                <input class="form-check-input mt-0" style="font-size: 1.5rem" role="switch" type="checkbox" id="Meal" {$isReserve['Meal']}>
                             </div>
                         </div>
                     </div>
                     <div class="row justify-content-between align-items-center mt-2 p-1">
-                        <h4 class="col-auto" id="total">$ 0</h4>
+                        <h4 class="col-auto" id="total">$ {$isReserve['total']}</h4>
                         <div class="col-auto">
                         <div class="form-check">
-                            <button type="button" class="btn btn-primary btn-rounded" id="checkout"><i class="fa-solid fa-cart-shopping me-2"></i>{$Text['Reserve']}</button>
+                            <button type="button" class="btn btn-primary btn-rounded" id="checkout" {$isReserve['button']}><i class="fa-solid fa-cart-shopping me-2"></i>{$Text['Reserve']}</button>
                             </div>
                         </div>
                     </div>
@@ -263,19 +299,49 @@ body;
     }
 
     function post(array $data): array {
-        return $data;
+        global $auth;
+
+        //檢查是否符合要求
+        if ($data['Economy'] <= 0 && $data['Business'] <= 0) return array(
+            'code' => 400,
+            'Title' => "Reserve Failed",
+            'Message' => "Failed to process request, please try again later"
+        );
+
+        $stmt = $this->sqlcon->prepare("INSERT INTO Reserve (ID, UUID, Business, Economy, Meal) VALUES (?, ?, ?, ?, ?)");
+        $stmt->bind_param('isiii', $this->upPath[0], $auth->userdata['UUID'], $data['Business'], $data['Economy'], $data['meal']);
+        if (!$stmt->execute()) return array(
+            'code' => 400,
+            'Title' => showText("Flight.Content.Fail.0"),
+            'Message' => showText("Flight.Content.Fail.1")
+        );
+
+        //check
+        if ($stmt->affected_rows >= 1) {
+            return array(
+                'code' => 200,
+                'Title' => showText("Flight.Content.Success.0"),
+                'Message' => showText("Flight.Content.Success.1")
+            );
+        } else {
+            return array(
+                'code' => 400,
+                'Title' => showText("Flight.Content.Fail.0"),
+                'Message' => showText("Flight.Content.Fail.1")
+            );
+        }
     }
 
     function path(): string {
-        return "<li><span><a href='/panel/'>" . showText("index.home") . "</a></span></li><li><span><a href='/panel/search/'>".showText('Search.Head')."</a></span></li><li><span>".strtoupper($this->flight)."</span></li>";
+        return "<li><span><a href='/panel/'>" . showText("index.home") . "</a></span></li><li><span><a href='/panel/search/'>" . showText('Search.Head') . "</a></span></li><li><span>" . strtoupper($this->flight) . "</span></li>";
     }
 
     public function get_Title(): string {
-        return strtoupper($this->flight)." ".showText('Flight.Title');
+        return strtoupper($this->flight) . " " . showText('Flight.Title');
     }
 
     public function get_Head(): string {
-        return showText('Flight.Head')." ".strtoupper($this->flight);
+        return showText('Flight.Head') . " " . strtoupper($this->flight);
     }
 
 
