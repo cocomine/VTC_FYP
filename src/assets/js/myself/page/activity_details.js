6d2f0b0e71cea9e0ef2ab09cd57ced4e5ba04ef9
Ka Tung LEUNG <michaelleung6884@gmail.com>
2023-01-25 13:55:15 +0800

    activity_details.js setting

---
+++ src/assets/js/myself/page/activity_details.js
@@ -0,0 +1,14 @@
+define(['jquery', 'media-select', 'myself/datepicker'], function (jq, media_select) {
+
+    $('#select').click(function () {
+        media_select.select_media((ids) => {
+            ids.forEach((id)=>{
+                $('#show').append(`<img src="/panel/api/media/${id}" alt="${id}"/>`)
+            })
+        });
+    })
+
+    $('#abc')[0].disableDate = ["01-11-2023", "01-13-2023"]
+    $('#ccc')[0].disableDate = ["01-11-2023", "01-13-2023"]
+    $('#ccc')[0].drawDatePicker()
+})
