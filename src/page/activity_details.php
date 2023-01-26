23fd550b983ac194a68cb208732d320690a15b9d
Ka Tung LEUNG <michaelleung6884@gmail.com>
2023-01-26 16:09:19 +0800

    activity_details Timetable&image

--- src/page/activity_details.php
+++ src/page/activity_details.php
@@ -2,6 +2,8 @@
 
 namespace page;
 
+use mysqli;
+
 class activity_details implements \cocomine\IPage {
 
     private array $UpPath;
@@ -46,31 +48,81 @@ class activity_details implements \cocomine\IPage {
         ));
 
         return <<<body
-    <pre id="media-select-LangJson" class="d-none">$LangJson</pre>
-    
-    <link rel="stylesheet" href="/panel/assets/css/myself/media-select.css">    
-    <link rel="stylesheet" href="/panel/assets/css/myself/datetimepicker.css">
-    
-    <div>
-        <div id="show"></div>
-        <button id="select">select image</button>
-        <div class="date-picker">
-            <input type="date" class="date-picker-toggle" min="01-10-2023" max="01-20-2023" id="abc">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+<link href="https://getbootstrap.com/docs/5.2/assets/css/docs.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"> </script>
+    <div class="center">
+        <p class="title">迪欣湖</p>
+
+        <div style="width:50%; float:left; border-style: ridge;" class="">
+            <p class="normal_text"><a href="#" id="show_detail" data-bs-target="#exampleModal" data-bs-toggle="modal">營業時間:</a> 9:00 - 19:00</p>
+            <p class="normal_text">地址: 香港竹篙灣</p>
+
+            <!-- Message_deatil_show -->
+            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
+                aria-hidden="true" >
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="exampleModalLabel">開放時間</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            週一-週日：9:00-19:00
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Message_deatil_show -->
         </div>
-        <div class="date-picker date-picker-inline">
-            <input type="date" class="date-picker-toggle" id="ccc">
-            <div class="date-calendar"></div>
+
+        <!-- Carousel -->
+        <div style="width:50%; float:right;" class="">
+            <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
+                <div class="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0"
+                        class="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
+                        aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
+                        aria-label="Slide 3"></button>
+                </div>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="img\img1.jpg" class="d-block w-100" alt="" width="200" height="350">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="img\img2.jpg" class="d-block w-100" alt="" width="200" height="350">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="img\img3.jpg" class="d-block w-100" alt="" width="200" height="350">
+                    </div>
+                </div>
+
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
+                    data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
+                    data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
         </div>
+        <!-- Carousel End-->
+
     </div>
+
     <script>
-        require.config({
-            paths:{
-                'media-select': 'myself/media-select',
-                'media-select.upload': 'myself/media-select.upload',
-            }
-        })
-        loadModules(['media-select', 'media-select.upload', "myself/page/test", 'myself/datepicker'])
+        $(document).ready(function () {
+        });
     </script>
+    
     body;
 
     }
