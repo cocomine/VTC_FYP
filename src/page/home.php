f2b3f10d0e1f34b06ed21e85c1cd0bbe7ca98ab5
Lamson Yeung <supertramp1026@gmail.com>
2023-01-26 22:50:10 +0800

    Carousel Updated

--- src/page/home.php
+++ src/page/home.php
@@ -43,23 +43,40 @@ class home implements IPage {
 <div class='col-12 mt-4'>
     <div class="card">
         <div class="card-body">
-            <div id="carouselExample" class="carousel slide">
+            <div id="carouselExampleCaptions" class="carousel slide">
+              <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+              </div>
               <div class="carousel-inner">
                 <div class="carousel-item active">
                   <img src="/assets/images/background/skiing-back.jpg" class="d-block w-100" alt="Skiing">
+                  <div class="carousel-caption d-none d-md-block">
+                    <button type="button" class="btn btn-primary btn-lg">See More</button>
+                    <h5>Into white world</h5>
+                  </div>
                 </div>
                 <div class="carousel-item">
                   <img src="/assets/images/background/diving-back.jpg" class="d-block w-100" alt="Diving">
+                  <div class="carousel-caption d-none d-md-block">
+                    <button type="button" class="btn btn-primary btn-lg">See More</button>
+                    <h5>Close to inhabitants of the sea</h5>
+                  </div>
                 </div>
                 <div class="carousel-item">
                   <img src="/assets/images/background/climbing-back.jpg" class="d-block w-100" alt="Climbing">
+                  <div class="carousel-caption d-none d-md-block">
+                    <button type="button" class="btn btn-primary btn-lg">See More</button>
+                    <h5>Rock and Roll</h5>
+                  </div>
                 </div>
               </div>
-              <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+              <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Previous</span>
               </button>
-              <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+              <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Next</span>
               </button>
