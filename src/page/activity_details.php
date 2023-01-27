590e1c91447c4526eccd50a48dc66b1b20802343
Ka Tung LEUNG <michaelleung6884@gmail.com>
2023-01-27 21:15:56 +0800

    test2

--- src/page/activity_details.php
+++ src/page/activity_details.php
@@ -187,8 +187,7 @@ class activity_details implements \cocomine\IPage {
                     <div class="carousel-item">
                         <img src="/assets/images/icon/Logo-big.png" class="d-block w-100" alt="" width="250" height="415">
                     </div>
-                </div>
-                
+                </div>           
 
                 <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
                     data-bs-slide="prev">
