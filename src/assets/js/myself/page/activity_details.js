87b15fa26d2b2cd10b512767c4c5c44f4085b8c7
Ka Tung LEUNG <michaelleung6884@gmail.com>
2023-02-19 14:09:19 +0800

    activity_details.js create && link

--- src/assets/js/myself/page/activity_details.js
+++ src/assets/js/myself/page/activity_details.js
@@ -1,14 +1,54 @@
-define(['jquery', 'media-select', 'myself/datepicker'], function (jq, media_select) {
+define(['jquery'], function (jq) {
 
-    $('#select').click(function () {
-        media_select.select_media((ids) => {
-            ids.forEach((id)=>{
-                $('#show').append(`<img src="/panel/api/media/${id}" alt="${id}"/>`)
-            })
+    $(document).ready(function () {
+        let num;
+        let total = $(".Price_txt").text();
+        $("#ti-plus").click(function () {
+            num = parseInt($(".txt_Num").text()) + 1;
+            $(".txt_Num").text(num);
+
+            $(".Price_txt").text(parseInt(total) * num);
         });
-    })
 
-    $('#abc')[0].disableDate = ["01-11-2023", "01-13-2023"]
-    $('#ccc')[0].disableDate = ["01-11-2023", "01-13-2023"]
-    $('#ccc')[0].drawDatePicker()
+        $("#ti-minus").click(function () {
+            if (parseInt($(".txt_Num").text()) > 1) {
+                num = parseInt($(".txt_Num").text()) - 1;
+                $(".txt_Num").text(num);
+
+                $(".Price_txt").text(parseInt(total) * num);
+            }
+        });
+
+
+        $("#bt_less_3").click(function(){
+            $("#card2").hide();
+            $("#card1").hide();
+            $("#Comment_Card").hide();
+        });
+
+        $("#bt_all").click(function(){
+            $("#card2").show();
+            $("#card1").show();
+            $("#Comment_Card").show();
+
+        });
+
+        $("#bt_over_3").click(function(){
+            $("#card2").show();
+            $("#card1").show();
+            $("#Comment_Card").show();
+
+        });
+
+        $("#bt_over_4").click(function(){
+            $("#card2").show();
+            $("#card1").show();
+            $("#Comment_Card").show();
+
+        });
+
+    });
+    // SetDate
+    const todayDate = new Date().toISOString().slice(0, 10);
+    document.getElementById("datePicker").min = todayDate;
 })
--- src/page/activity_details.php
+++ src/page/activity_details.php
@@ -381,58 +381,7 @@ class activity_details implements \cocomine\IPage {
     
     <!-- JQuery -->
     <script>
-        $(document).ready(function () {
-            let num;
-            let total = $(".Price_txt").text();
-            $("#ti-plus").click(function () {
-                num = parseInt($(".txt_Num").text()) + 1;
-                $(".txt_Num").text(num);
-
-                $(".Price_txt").text(parseInt(total) * num);
-            });
-
-            $("#ti-minus").click(function () {
-                if (parseInt($(".txt_Num").text()) > 1) {
-                    num = parseInt($(".txt_Num").text()) - 1;
-                    $(".txt_Num").text(num);
-
-                    $(".Price_txt").text(parseInt(total) * num);
-                }
-            });
-            
-            
-            $("#bt_less_3").click(function(){
-                $("#card2").hide();
-                $("#card1").hide();
-                $("#Comment_Card").hide();
-            });
-            
-            $("#bt_all").click(function(){
-                $("#card2").show();
-                $("#card1").show();
-                $("#Comment_Card").show();
-            
-            });
-            
-            $("#bt_over_3").click(function(){
-                $("#card2").show();
-                $("#card1").show();
-                $("#Comment_Card").show();
-            
-            });
-            
-            $("#bt_over_4").click(function(){
-                $("#card2").show();
-                $("#card1").show();
-                $("#Comment_Card").show();
-            
-            });
-            
-        });
-            // SetDate
-            var todayDate = new Date().toISOString().slice(0, 10);
-            document.getElementById("datePicker").min = todayDate;
-        
+        loadModules(['myself/page/activity_details'])
     </script>
     
     body;
