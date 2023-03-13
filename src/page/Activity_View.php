059e9812d1f08527210187d3ac4040983250f9e6
Ka Tung LEUNG <michaelleung6884@gmail.com>
2023-03-13 21:23:07 +0800

    Setting

--- src/page/Activity_View.php
+++ src/page/Activity_View.php
@@ -2,7 +2,56 @@
 
 namespace page;
 
-class Activity_View
+class Activity_View implements IPage
 {
 
+    private mysqli $sqlcon;
+
+    /**
+     * home constructor.
+     * sql連接
+     * @param $sqlcon
+     */
+    function __construct($sqlcon) {
+        $this->sqlcon = $sqlcon;
+    }
+
+    /* 是否有權進入 */
+    function access(bool $isAuth, int $role, bool $isPost): int {
+        return 200;
+    }
+
+    /* 輸出頁面 */
+    function showPage(): string {
+
+        //$Text = showText('index.Content');
+
+        /* json 語言 */
+        //$jsonLang = json_encode(array());
+
+        //return ;
+
+    }
+
+    /* POST請求 */
+    function post(array $data): array {
+        global $auth;
+
+        return array();
+    }
+
+    /* path輸出 */
+    function path(): string {
+        return '<li class="breadcrumb-item active">' . showText("index.home") . '</li>';
+    }
+
+    /* 取得頁面標題 */
+    public function get_Title(): string {
+        return showText('index.title');
+    }
+
+    /* 取得頁首標題 */
+    public function get_Head(): string {
+        return showText("index.home");
+    }
 }
