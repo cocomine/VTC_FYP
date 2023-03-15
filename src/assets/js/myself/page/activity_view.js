2c8b3f789dd4c8048dc1ac354b52df1a94c183b1
Ka Tung LEUNG <michaelleung6884@gmail.com>
2023-03-15 15:48:27 +0800

    TESTING setting

--- src/assets/js/myself/page/activity_view.js
+++ src/assets/js/myself/page/activity_view.js
@@ -1,5 +1,54 @@
 
 define([ 'jquery', 'toastr', 'moment', 'datatables.net', 'datatables.net-bs5', 'datatables.net-responsive', 'datatables.net-responsive-bs5' ], function (jq, toastr, moment){
-
-
+    const table = $('#dataTable').DataTable({
+        responsive: true,
+        ajax: {
+            url: '/panel/reserve/',
+            type: 'POST',
+            data: function (d){
+                return JSON.stringify(d);
+            }
+        },
+        language: {
+            loadingRecords: `<div id="pre-submit-load" style="height: 20px; margin-top: -4px"> <div class="submit-load"><div></div><div></div><div></div><div></div></div> </div>`,
+            url: $('#datatables_lang_url').text()
+        },
+        order: [
+            [ 1, 'desc' ]
+        ],
+        columns: [
+            {
+                data: 'name',
+                render: (data, type, row) => {
+                    if (type === 'display'){
+                        return `<div class="row">
+                                    <div class="col-auto">
+                                        <a href="/panel/reserve/${row.ID}">
+                                            <div class="ratio ratio-16x9" style="width: 160px;">
+                                                <img src="/panel/api/media/${row.thumbnail}" alt="${row.thumbnail}" class="w-auto mh-100 h-auto">
+                                            </div>
+                                        </a>
+                                    </div>
+                                    <div class="col">
+                                        <a href="/panel/reserve/${row.ID}">${data}</a><br>
+                                        <p class="text-secondary" style="max-width: 300px">${row.summary}</p>
+                                    </div>
+                                </div>`;
+                    }else{
+                        return data + ';' + row.summary;
+                    }
+                }
+            },
+            {
+                data: 'plan',
+                render: (data, type) => {
+                    if (type === 'display'){
+                        return data ? data.map((value) => `<b>${value.plan_name}:</b> <code class="bg-light">${value.total}</code>`).join('<br>') : "沒有任何活動計劃"
+                    }else{
+                        return data ? data.map((value) => value.plan_name + ',' +value.total).join(';') : "沒有任何活動計劃";
+                    }
+                }
+            },
+        ]
+    });
 });
--- src/page/activity_view.php
+++ src/page/activity_view.php
@@ -69,7 +69,72 @@ class activity_view implements IPage
 
     /* POST請求 */
     function post(array $data): array {
+        /* 檢視審核活動 */
+        if(sizeof($this->upPath) > 0){
+            return $this->reservePost->post($data);
+        }
+
         global $auth;
+        $output = array();
+
+        /* 取得該用戶建立的活動給參與人數 */
+        /* */
+        $stmt = $this->sqlcon->prepare("SELECT ID, thumbnail, summary, name FROM Event WHERE UUID = ?");
+        $stmt->bind_param('s', $auth->userdata['UUID']);
+        if (!$stmt->execute()) {
+            return array(
+                'code' => 500,
+                'Title' => 'Database Error!',
+                'Message' => $stmt->error,
+            );
+        }
+
+        /* get result */
+        $result = $stmt->get_result();
+        while ($row = $result->fetch_assoc()){
+            $temp = array(
+                'ID' => $row['ID'],
+                'thumbnail' => $row['thumbnail'],
+                'summary' => $row['summary'],
+                'name' => $row['name'],
+                'plan' => null
+            );
+
+            /* get plan */
+            $stmt->prepare("SELECT Event_ID, plan, (SELECT plan_name FROM Event_plan WHERE plan_ID = Event_schedule.plan) AS `plan_name` FROM Event_schedule WHERE Event_ID = ? ORDER BY LENGTH(plan_name)");
+            $stmt->bind_param('i', $row['ID']);
+            if (!$stmt->execute()) {
+                return array(
+                    'code' => 500,
+                    'Title' => 'Database Error!',
+                    'Message' => $stmt->error,
+                );
+            }
+
+            /* get schedule */
+            $schedule_result = $stmt->get_result();
+            while ($row = $schedule_result->fetch_assoc()){
+                $stmt->prepare("SELECT COALESCE(SUM(plan_people), 0) AS `total` FROM Book_event_plan WHERE event_schedule IN(SELECT Schedule_ID FROM Event_schedule WHERE plan = ? AND Event_ID = ?)");
+                $stmt->bind_param('ii', $row['plan'], $row['Event_ID']);
+                if (!$stmt->execute()) {
+                    return array(
+                        'code' => 500,
+                        'Title' => 'Database Error!',
+                        'Message' => $stmt->error,
+                    );
+                }
+
+                /* get result */
+                $schedule_row = $stmt->get_result()->fetch_assoc();
+                $temp['plan'][] = array(
+                    'plan_name' => $row['plan_name'],
+                    'total' => $schedule_row['total']
+                );
+            }
+
+            $output[] = $temp;
+        }
+        return array('data' => $output);
 
         return array();
     }
