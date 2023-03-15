eebe573f2a20bba856491adae04474a2df15030f
Ka Tung LEUNG <michaelleung6884@gmail.com>
2023-03-15 15:22:54 +0800

    div setting

--- src/page/activity_view.php
+++ src/page/activity_view.php
@@ -32,19 +32,23 @@ class activity_view implements IPage
             <pre class="d-none" id="datatables_lang_url">$datatables_lang_url</pre>
             <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.2/css/dataTables.bootstrap5.min.css"/>
             <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/responsive/2.4.0/css/responsive.bootstrap5.min.css"/>
-            <div class="col-12">
-                <div class="card">
-                    <div class="card-body">
-                        <div class="data-tables datatable-primary">
-                            <table id="dataTable" class="w-100">
-                                <thead class="text-capitalize">
-                                    <tr>
-                                        <th>活動</th>
-                                        <th>活動計劃 / 預約人數</th>
-                                    </tr>
-                                </thead>
-                                <tbody></tbody>
-                            </table>
+            <div class="container-sm">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="data-tables datatable-primary">
+                                    <table id="dataTable" class="w-100">
+                                        <thead class="text-capitalize">
+                                            <tr>
+                                                <th>活動</th>
+                                                <th>活動計劃 / 預約人數</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody></tbody>
+                                    </table>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -61,7 +65,7 @@ class activity_view implements IPage
 
     /* path輸出 */
     function path(): string {
-        return '<li class="breadcrumb-item active">' . showText("index.home") . '</li>';
+        return '<li class="breadcrumb-item active"><a href="/home">' . showText("index.home") . '</a></li>'." >  <li><span>查看預約</span></li>";
     }
 
     /* 取得頁面標題 */
