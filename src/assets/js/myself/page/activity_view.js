c77c67685369fe4c904884c4fc14ca2c0baf0870
Ka Tung LEUNG <michaelleung6884@gmail.com>
2023-03-15 15:34:25 +0800

    activity_view.js definition

---
+++ src/assets/js/myself/page/activity_view.js
@@ -0,0 +1,5 @@
+
+define([ 'jquery', 'toastr', 'moment', 'datatables.net', 'datatables.net-bs5', 'datatables.net-responsive', 'datatables.net-responsive-bs5' ], function (jq, toastr, moment){
+
+
+});
