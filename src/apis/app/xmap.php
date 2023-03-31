47f14c0faa117faa07f989f6672ae554ea3741d6
cocomine <roguish.6888467468@gmail.com>
2023-04-01 00:58:25 +0800

    xmap app api rate

--- src/apis/app/xmap.php
+++ src/apis/app/xmap.php
@@ -38,14 +38,26 @@ class xmap implements \cocomine\IApi {
     public function post(?array $data) {
         header("Content-Type: text/json");
         $stmt = $this->sqlcon->prepare("SELECT ID, name, summary, thumbnail, longitude, latitude FROM Event WHERE longitude BETWEEN ? AND ? AND latitude BETWEEN ? AND ? AND state = 1 ORDER BY create_time DESC");
-        $stmt->bind_param("dddd", $data['northEast']['longitude'], $data['southWest']['longitude'], $data['northEast']['latitude'], $data['southWest']['latitude']);
+        $stmt->bind_param("dddd", $data['southWest']['longitude'], $data['northEast']['longitude'], $data['southWest']['latitude'], $data['northEast']['latitude']);
         if (!$stmt->execute()) {
             http_response_code(500);
             echo json_encode(['status' => 500, 'Message' => $stmt->error, 'Title' => showText('Error_Page.something_happened')]);
         }
 
+        $data = [];
         $result = $stmt->get_result();
-        echo json_encode(['code' => 200, 'data' => $result->fetch_all(MYSQLI_ASSOC)]);
+        while ($row = $result->fetch_assoc()) {
+            $stmt->prepare("SELECT ROUND(SUM(r.rate)/COUNT(*), 1) AS 'rate', COUNT(*) AS 'total' FROM Book_review r, Book_event b WHERE r.Book_ID = b.ID AND event_ID = ?");
+            $stmt->bind_param("i", $row['ID']);
+            $stmt->execute();
+            $rate = $stmt->get_result()->fetch_assoc();
+            $row['rate'] = $rate['rate'] ?? 5;
+            $row['total'] = $rate['total'];
+            $data[] = $row;
+        }
+
+
+        echo json_encode(['code' => 200, 'data' => $data]);
     }
 
     /**
